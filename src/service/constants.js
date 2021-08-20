befc2da419a3e97e9e5ec21271e7a199d3698c2a
Zyuzin Vitaly <juwain@yandex.ru>
2021-08-20 17:58:15 +0300

    Меняет урлы временно

--- src/service/constants.js
+++ src/service/constants.js
@@ -1,5 +1,5 @@
-const indexURL = 'https://raw.githubusercontent.com/w3c/webref/master/ed/index.json';
-const specURL = 'https://raw.githubusercontent.com/w3c/webref/master/ed/dfns/';
+const indexURL = 'https://raw.githubusercontent.com/w3c/webref/master/tr/index.json';
+const specURL = 'https://raw.githubusercontent.com/w3c/webref/master/tr/dfns/';
 
 export {
   indexURL,
