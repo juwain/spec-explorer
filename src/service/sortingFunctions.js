602e68f29ef401b922159e954349e8b0ef605808
Zyuzin Vitaly <juwain@yandex.ru>
2021-08-23 18:08:10 +0300

    Правит функцию

--- src/service/sortingFunctions.js
+++ src/service/sortingFunctions.js
@@ -2,14 +2,7 @@ const sortByAlphabet = (a, b) => {
   const titleA = a.title.toUpperCase();
   const titleB = b.title.toUpperCase();
 
-  if (titleA < titleB) {
-    return -1;
-  }
-  if (titleA > titleB) {
-    return 1;
-  }
-
-  return 0;
+  return (titleA < titleB) ? -1 : 1;
 }
 
 const sortByDate = (a, b) => {
